6815be86a029665ec9531194ab1c9a228065603a
valdir-ti <valdir.ti@gmail.com>
2020-07-15 14:14:52 -0300

    Get all movies and build rows

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import Row from './Row';
 import './App.css';
+
+import Row from './Row';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
-      <h1>Netflix Clone</h1>
-      <Row title="NETFLIX ORIGINALS"/>
-      <Row title="Trending Now"/>
+      <h1>Netflix Clone</h1>      
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Actions Movies" fetchUrl={requests.fetchActionsMovies}/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
     </div>
   );
 }
--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
+import './Row.css';
 
-// import { Container } from './styles';
+const baseUrl = "https://image.tmdb.org/t/p/original/";
 
-function Row({title}) {
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(()=>{
+    async function fetchData(){
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  console.table(movies);
+
   return (
-    <div>
+    <div className="row">
       
       {/* Title */}
       <h2>{title}</h2>
 
-      {/* Container -> posts */}
+      <div className="row_posters">
+        {/* Posters */}
+        {movies.map(movie => (
+          <img 
+            key={movie.id}
+            className="row_poster" 
+            src={`${baseUrl}${movie.poster_path}`} 
+            alt={movie.name}
+          />
+        ))}
+      </div>
       
+      {/* Container -> posts */}
 
     </div>
   );
